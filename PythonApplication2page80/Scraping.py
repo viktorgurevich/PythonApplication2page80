92c473cbc0857b97c2ac85403408771fc99920a9
viktor <viktor@WD1000WIN10>
2021-08-16 20:18:55 -0400

    artificial header partially help

--- PythonApplication2page80/Scraping.py
+++ PythonApplication2page80/Scraping.py
@@ -19,3 +19,13 @@ print(r.status_code)
 print(r.reason)
 # What were the HTTP response headers?
 print(r.headers)
+
+#-----------------------------------------------------------------------------------------------
+import requests
+#url = 'http://www.webscrapingfordatascience.com/usercheck/'  #This works
+url = 'https://en.wikipedia.org/w/index.php\?title=List_of_Game_of_Thrones_episodes&oldid=802553687'    #This doesnot
+my_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' + ' (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+
+r = requests.get(url, headers=my_headers)
+print(r.text)
+print(r.request.headers)
